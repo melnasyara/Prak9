81b40d51b2540d01edc3acb44d6e635d344e5df9
melna.syara <melnasyarawati@gmail.com>
2023-07-12 16:44:56 +0700

    UASTERBARU

--- Praktek 9/Main.java
+++ Praktek 9/Main.java
@@ -1,29 +1,55 @@
 public class Main {
     public static void main(String [] args) {
-        
+
         Buku buku1 = new Buku("0001", "Pemrogramman Java");
         Buku buku2 = new Buku("0002", "Pemrogramman C++");
         BukuLuar buku3 = new BukuLuar("0003", "Pemrogramman Python");
-    
+
         Petugas petugas1 = new Petugas("Alex", "P001");
         Peminjam peminjam1 = new Mahasiswa();
-        peminjam1.setNamaPeminjam("Huda");
-        peminjam1.setKodePeminjam("M001");
-        
-       peminjam1.doLogin();
-       //peminjam1.doLogout();
+        peminjam1.setNamaPeminjam("Cinta");
+        peminjam1.setKodePeminjam("M002");
+
+        peminjam1.doLogin();
 
         try{
         Peminjaman peminjaman1 = new Peminjaman(peminjam1, petugas1, buku3);
-
+        Denda denda = new Denda(7);
         System.out.println("Nama Peminjam :"+peminjaman1.getPeminjam().getNamaPeminjam());
         System.out.println("Kode Peminjam :"+peminjaman1.getPeminjam().getKodePeminjam());
         System.out.println("Nama Petugas :"+peminjaman1.getPetugas().getNamaPetugas());
         System.out.println("Nama Buku :"+peminjaman1.getBuku().getNamaBuku());
+        System.out.println("Tagihan denda: "+denda.getTagihan());
+        denda.addAntiDenda();
+        System.out.println("=== Denda dihapuskan ! ===");
+        System.out.println("Tagihan denda: "+denda.getTagihan());
+        denda.addAntiDenda();
+        System.out.println("=== Denda ditambahkan ===");
+        System.out.println("Tagihan denda: "+denda.getTagihan());
         }catch (Exception e){
             System.out.println("Terjadi Kesalahan"+e.getMessage());
         } finally {
             System.out.println("Program Selesai");
         }
+    }
+}
+public class Denda {
+    private static final int TARIF_DENDA = 10000;
+    private int hariTerlambat;
+
+    public Denda(int hariTerlambat) {
+        this.hariTerlambat = hariTerlambat;
+    }
+
+    public int getHariTerlambat() {
+        return hariTerlambat;
+    }
+
+    public int getTagihan() {
+        return TARIF_DENDA * hariTerlambat;
+    }
+
+    public void addAntiDenda() {
+        this.hariTerlambat = 0;
     }
 }
